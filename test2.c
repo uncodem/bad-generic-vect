48220258b2f24f3adab7792c94e03a8335971b21
aiden <emurex185@gmail.com>
2021-01-29 21:30:01 +0800

    Changed values around test.c

--- test2.c
+++ test2.c
@@ -5,8 +5,8 @@
 
 int main() {
 	char *first = "Hello, ";
-	t_(char, vector) string = t_(char, initp)(first);
-	t_(char, join)(&string, t_(char, initp)("World!"), -1);
+	t_(char, vector) string = t_(char, initnp)(first, sizeof(first)-1);
+	t_(char, join)(&string, t_(char, initp)("World!"), 0);
 	printf("%zu | %s\n", string.size, string.data);
 	return 0;
 }
